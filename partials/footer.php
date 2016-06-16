00e44a00dc00ec81b96ae812ae56710d82eac6ee
brokenhd <michaelastarks@gmail.com>
2016-06-15 19:47:18 -0700

    adding footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,26 @@
-		<footer id="footer-nav">
+		<div class="footer">
 
-		</footer>
+			<div class="content">
+				<div class="col newsletter">
+					<h3>Don't miss an update!</h3>
+					<form>
+						<input type="email" placeholder="Email"/>
+						<input type="submit" value="Sign Up"/>
+					</form>
+				</div>
+
+				<div class="col social">
+					<h3>Remember to follow!</h3>
+					<a href="<?= $global['social']['facebook']; ?>"><img src="/img/facebook.png"></a>
+					<a href="<?= $global['social']['twitter']; ?>"><img src="/img/twitter.png"></a>
+					<a href="<?= $global['social']['youtube']; ?>"><img src="/img/youtube.png"></a>
+					<a href="<?= $global['social']['patreon']; ?>"><img src="/img/patreon.png"></a>
+				</div>
+			</div>
+			<div class="copyright">
+				&copy; 2016 Nightown
+			</div>
+		</div>
 	</div>  <!-- end #main  -->
 
 	<script src="/js/vendor/require-jquery.js"></script>
